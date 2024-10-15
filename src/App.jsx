399961612e559f7b719e65d080f60f4fbdbca641
Fathima Suneer <fathimasuneer0375@gmail.com>
2024-10-15 14:00:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import { Container,Row,Col, Spinner } from 'react-bootstrap';
+import './App.css'
+import Headernav from './headernav';
+import Productitems from './productitems';
+import Signup from './usersignup';
+import {Routes,Route} from 'react-router-dom';
+import { lazy,Suspense } from 'react';
+import About from './about';
+function App() {
+ const Home=lazy(()=>import('./productitems'))
+ const Sign=lazy(()=> import('./usersignup'))
+ const Abt=lazy(()=> import('./about'))
+ const Parl=lazy(()=> import('./parallexex'))
+ const UserH=lazy(()=> import('./userhome'))
+  return (
+    <>
+
+    <Suspense fallback={ 
+      <div style={{width:'60%',margin:'0 auto', display:'flex',justifyContent:'center'}}>
+      <Spinner   animation="border" variant='danger'/>
+      </div>
+      }>
+        <Routes>
+          <Route path="/" element={<Home/>}/> 
+          <Route path="/signup" element={<Sign/>}/>
+          <Route path="/about" element={<Abt/>}/>
+          <Route path="/userhome/*" element={<UserH/>}/>
+        </Routes>
+    </Suspense>
+    
+     
+    </>
+  )
+}
+
+export default App
+
---
+++ src/login.jsx
@@ -0,0 +1,59 @@
+import { useState,useRef,useEffect } from "react";
+import {useNavigate} from 'react-router-dom'
+export default function Login(){
+  
+    const refElement=useRef(null)
+    const nav=useNavigate()
+    const [login,setLogin]=useState({})
+    const [msg,setMsg]=useState("")
+   
+    const changeValue=(e)=>{ 
+        setLogin({...login,[e.target.name]:e.target.value})
+
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        console.log(login)
+        if(login.email=="example@gmail.com" && login.password=="123"){
+            console.log("login success")
+            nav("/userhome")
+        }
+        else{
+            console.log("Not Success")
+             
+            setMsg("Error in Login")
+            nav("/")
+        }
+
+    }
+    return(
+        <>
+          <Container>
+            <Row className="justify-content-center">
+                <Col ref={refElement}  lg={12}>
+                <h1  className="text-center text-info">Login Here!</h1>
+                <h2>{msg}</h2>
+                </Col>
+                
+             
+                <Col lg={12} className="p-3">
+                <FloatingLabel label="Email" className="m-3">
+                    <Form.Control onChange={changeValue} name="email" type="email" placeholder="your emailid"/>
+                </FloatingLabel>
+                </Col>
+                <Col lg={12} className="p-3">
+                <FloatingLabel label="Password" className="m-3">
+                    <Form.Control  onChange={changeValue}name="password" type="password" placeholder="your password"/>
+                </FloatingLabel>
+                </Col>
+                <Col lg={12} className="mt-3 p-3" align="center">
+                <Button variant="success" onClick={handleSubmit}>
+                  Login
+                </Button>
+                </Col>
+            </Row>
+            </Container>
+        
+        </>
+    )
+}
